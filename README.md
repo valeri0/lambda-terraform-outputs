d0f4dd6e787b6094cac0a4a2dd86e1f2d67d0cdc
valeri0 <mihaila.andrei95@gmail.com>
2022-11-02 21:33:42 +0200

    - Adressed corner cases in the lambda function - Updated README

--- README.md
+++ README.md
@@ -4,5 +4,50 @@ Small terraform project which deploys a lambda that is used for retrieving terra
 
 ## Setup
 
-1. Create an aws profile in `~/.aws/credentials` file.
-2. Export the profile name using `export TF_VAR_AWS_PROFILE=<profile_name>`
+1. Create an aws profile in `~/.aws/credentials` file with the account access key and secret key.
+2. Export the profile name using `export AWS_PROFILE=<profile_name>`
+3. Configure the `terraform.tfvars` with the following values:
+   ```
+   lambda_name = <value>
+   input_s3_bucket_name = <value>
+   ```
+
+## Running the Lambda
+
+The lambda function returns outputs from the `terraform.tfstate` file from the `input_s3_bucket_name` which is configured in the `terraform.tfvars`.
+
+If no input is given for the lambda invocation, then it will automatically provide the entire `outputs` section from the state file.
+
+Otherwise, if an input that respects the following format:
+
+```
+{
+    "specific_output": "<output_name>"
+}
+```
+
+is provided, then it will a return the following response:
+
+```
+{
+Response
+{
+  "statusCode": 200,
+    "body": {
+    "<specific_output_name>": {
+      "value": "<string>",
+      "type": "<type>"
+    }
+  }
+}
+}
+```
+
+If an output name which does not exist in the tfstate file is provided, then the Lambda function will return:
+
+```
+{
+  "statusCode": 404,
+  "body": "No such output has been found for: <specific_output_name>"
+}
+```
--- external/src/lambda_function.py
+++ external/src/lambda_function.py
@@ -1,3 +1,7 @@
+"""
+Lambda function which retrieves outputs from a remote terraform state file from S3
+"""
+
 import os
 import json
 import boto3
@@ -5,32 +9,35 @@ import boto3
 s3_client = boto3.client("s3")
 
 def lambda_handler(event, context):
-    
     # get bucket from environment variables
     bucket_name = os.environ['input_bucket']
 
     # get the input parameter from the lambda invocation
-    specific_output = event['specific_output'][0]
-    
+    specific_output = event.get('specific_output', None)
+
     # load the outputs section of the tfstate in memory
     outputs_section_from_tf_state = json.loads(read_tfstate_file(bucket_name))['outputs']
-    
-    return {
-        'statusCode' : 200,
-        'body': json.dumps(get_outputs(specific_output, outputs_section_from_tf_state))
-        }
 
+    code, body = get_outputs(specific_output, outputs_section_from_tf_state)
+    return {
+        'statusCode' : code,
+        'body': body
+        }
 
 def read_tfstate_file(bucket_name):
     # we will assume that in the bucket there will be only one terraform.tfstate file
     return s3_client.get_object(Bucket=bucket_name, Key='terraform.tfstate')["Body"].read()
-  
 
 def get_outputs(specific_output, all_outputs_from_state_file):
     
-    if specific_output is not None:
-        for output_item in all_outputs_from_state_file:
-            return {specific_output: all_outputs_from_state_file[specific_output]}
+    if specific_output is None:
+        # by default, if no specific output is provided, we will return all output values
+        return 200, all_outputs_from_state_file
     else:
-        # by default we will return all output values
-        return all_outputs_from_state_file
+       # check if required output does exist
+       if all_outputs_from_state_file.get(specific_output, None) is None:
+           return 404, 'No such output has been found for: ' + specific_output
+       
+       # return specific output
+       return 200, {specific_output: all_outputs_from_state_file[specific_output]}
+
